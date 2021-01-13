b4e9236c50c706d9a924eea72ecbc4c1910ac8b9
Mtwod <maximewong@hotmail.com>
2021-01-13 15:13:47 +0100

    Add exercise 6

---
+++ ex6.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+/*****************************
+ * VERIFY THE DATA
+ * ***************************/
+
+const hasOnlyNumbers = (array) => {
+  const notNumbers = array.filter((element) => !Number.isInteger(element));
+  if (notNumbers.length >= 1) return false;
+  return true; 
+};
+
+const westView = (numbers) => {
+  let toCheck = [...numbers];
+  let hasView = [];
+  
+  for (let i = toCheck.length - 1; i >= 0; i--) {
+    if (toCheck[i] > Math.max(...hasView)) hasView.push(toCheck[i]);
+  }
+
+  return hasView.length;
+}
+
+// Méthode synchrone
+try {
+  const data = fs.readFileSync(fileName, 'utf8');
+  const numbers = data.split(' ').map((number) => Number.parseInt(number));
+  if (!hasOnlyNumbers(numbers)) return console.error("The data is not valid!");
+
+  console.log('====================================');
+  console.log('BEGIN');
+  console.log(numbers);
+  console.log('====================================\n');
+
+  console.log(`Le nombre final est: ${westView(numbers)}`);
+
+  console.log('\n====================================');
+  console.log('END');
+  console.log('====================================');
+  
+} catch (error) {
+  console.error(error.message);
+}
