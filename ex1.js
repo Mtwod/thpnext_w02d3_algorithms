62d72ec2b5542b52858e7f961781100a4cc4db58
Mtwod <maximewong@hotmail.com>
2021-01-13 15:13:33 +0100

    Add exercise 1

---
+++ ex1.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+const fileName = process.argv[2];
+
+/*****************************
+ * VERIFY THE DATA
+ * ***************************/
+
+const hasOnlyNumbers = (array) => {
+  const notNumbers = array.filter((element) => !Number.isInteger(element));
+  if (notNumbers.length >= 1) return false;
+  return true; 
+};
+
+const sumPair = (numbers, total) => {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === total) return true;
+    }
+  }
+  return false;
+}
+
+// Méthode synchrone
+try {
+  const data = fs.readFileSync(fileName, 'utf8');
+  const numbers = data.split(' ').map((number) => Number.parseInt(number));
+  if (!hasOnlyNumbers(numbers)) return console.error("The data is not valid!");
+
+  console.log('====================================');
+  console.log('BEGIN');
+  console.log(numbers);
+  console.log('====================================\n');
+
+  console.log(sumPair(numbers, 17));
+  console.log(sumPair(numbers, 19));
+
+  console.log('\n====================================');
+  console.log('END');
+  console.log('====================================');
+  
+} catch (error) {
+  console.error(error.message);
+}
